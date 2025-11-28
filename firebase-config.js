b6d8e5899014f52f459f6f22e0da547dab46df1e
sytakadamiob <swannyaetoee@gmail.com>
2025-11-27 22:16:55 -0800

    chore: track firebase config for deployment

---
+++ firebase-config.js
@@ -0,0 +1,14 @@
+/**
+ * Copy this file to firebase-config.js and fill in your real Firebase config.
+ * firebase-config.js is git-ignored so your keys stay out of the repo.
+ */
+window.__FIREBASE_CONFIG__ = {
+  apiKey: "AIzaSyCPihYRxh0KQc_p7gFVW-yrMf5aXzPGJCc",
+  authDomain: "watchlist-app-defb4.firebaseapp.com",
+  projectId: "watchlist-app-defb4",
+  storageBucket: "watchlist-app-defb4.firebasestorage.app",
+  messagingSenderId: "87335175860",
+  appId: "1:87335175860:web:e97a5cbc6ca96284ea79e1",
+  measurementId: "G-CDP9KF0WB9"
+};
+
